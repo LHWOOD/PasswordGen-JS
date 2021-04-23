4018999efea5a2cf800438f5ff77ec18da49a4f3
LHWOOD <hutsonwood@gmail.com>
2021-04-22 23:57:52 -0600

    began to code the geneatePassword variable and started with the prompts and alerts.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 var lowercase = [
   "a",
   "b",
@@ -56,6 +57,7 @@ var uppercase = [
   "Y",
   "Z",
 ];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacter = [
   "!",
   "@",
@@ -70,6 +72,7 @@ var specialCharacter = [
   "+",
   "_",
 ];
+var possibleCode = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -79,5 +82,55 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function generatePassword() {
+  var passwordLength = prompt(
+    "How long do you want your Password to be? (Passwords must be between 8 and 128 characters long.)"
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Passwords must be between 8 and 128 characters long!");
+    generatePassword();
+  } else {
+    alert("You have selected " + passwordLength + " characters long");
+  }
+  var lowercaseChoice = confirm(
+    "Would you like your passcode to contain lowercase letters?"
+  );
+  if (lowercaseChoice === true) {
+    possibleCode.push(lowercase);
+    alert("Lowercase has been selected.");
+  }
+  var uppercaseChoice = confirm(
+    "Would you like your passcode to contain uppercase letters?"
+  );
+  if (uppercaseChoice === true) {
+    possibleCode.push(uppercase);
+    alert("Uppercase has been selected.");
+  }
+  var numberChoice = confirm(
+    "Would you like your passcode to contain numbers?"
+  );
+  if (numberChoice === true) {
+    possibleCode.push(numbers);
+    alert("Numbers have been selected.");
+  }
+  var specialCharacterChoice = confirm(
+    "Would you like your passcode to contain special characters?"
+  );
+  if (specialCharacterChoice === true) {
+    possibleCode.push(specialCharacter);
+    alert("Special characters have been selected.");
+  }
+  if (possibleCode == 0) {
+    alert("Your password must contain values!");
+    generatePassword();
+  }
+
+  var index = Math.floor(Math.random() * passwordLength.length);
+  var password = possibleCode[index];
+  console.log(index);
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("Click", generatePassword);
